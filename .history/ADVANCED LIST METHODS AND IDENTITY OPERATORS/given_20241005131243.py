ef279348469f8a82cc918b63e5148573ba4c59cf
britt pitts <bmarie893@gmail.com>
2024-10-05 13:31:40 -0500

    first commit

---
+++ .history/ADVANCED LIST METHODS AND IDENTITY OPERATORS/given_20241005131243.py
@@ -0,0 +1,14 @@
+#Problem Statement: You have two lists of student names. One list contains the names of students who have submitted their assignments, 
+# and the other list contains the names of students who attended the last class.
+
+#Task 1: Given the two lists:
+
+submitted = ["Alice", "Bob", "Charlie", "David"]
+attended = ["Charlie", "Eve", "Alice", "Frank"]
+#Find out if Alice submitted their assignment and attended class. 
+# HINT: How might the in keyword be of use here? And how can you check to see if Alice is in both submitted and attended in one if statement?
+def Alice 
+if Alice in attended and submitted:
+    print(f"Alice was in class and submitted her assingment!")
+else:
+    print(f"Alice was not in class or submitted assingment!")
---
+++ ADVANCED LIST METHODS AND IDENTITY OPERATORS/given.py
@@ -0,0 +1,14 @@
+#Problem Statement: You have two lists of student names. One list contains the names of students who have submitted their assignments, 
+# and the other list contains the names of students who attended the last class.
+
+#Task 1: Given the two lists:
+
+submitted = ["Alice", "Bob", "Charlie", "David"]
+attended = ["Charlie", "Eve", "Alice", "Frank"]
+#Find out if Alice submitted their assignment and attended class. 
+# HINT: How might the in keyword be of use here? And how can you check to see if Alice is in both submitted and attended in one if statement?
+#used geeks for geeks - was kind hard 
+completed_work = set(submitted).intersection(set(attended))
+
+for Alice in completed_work:
+    print("Alice did what was expected!")
